f552a87a5efa5f12f67a7e4c9b955e3576c77248
CaioAReis <Caio.almeida16.ca@gmail.com>
2020-07-06 18:47:39 -0300

    Knex #08-ORDER BY

--- Aulas/Knex JS/#07-UPDATE & #08-ORDER BY/index.js
+++ Aulas/Knex JS/#07-UPDATE & #08-ORDER BY/index.js
@@ -12,6 +12,13 @@ database.table('games').update({preco: 10.90}).where({id: 10}).then(() => {
     console.log(err);
 });
 
+//  ORDER BY
+database.select().from('games').orderBy('preco', 'asc').then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
+
 /*  UPDATE ->  .update(data, [returning]) OU .update(key, value, [returning]
 
     --> Para utilizar o UPDATE basta passar um JSON com os campos que desejamos alterar update({}):
@@ -23,4 +30,20 @@ database.table('games').update({preco: 10.90}).where({id: 10}).then(() => {
         EXEMPLO PARA UNICA ATUALIZAÇÃO:
 
             database.update('preco', 120).from('games').where({id: 12});
+*/
+
+/*  ORDER BY ->    .orderBy(column|columns, [direction])
+
+    --> Para ordenar uma consulta basta seguir o seguuinte exemplo:
+
+        asc --> PARA CRESCENTE
+        desc --> PARA DECRESCENTE
+
+    --> COLUNA ÚNICA:
+
+            database.select().from('games').orderBy('nome', 'desc');
+        
+        EQUIVALENTE:
+
+            SELECT * FROM games ORDER BY nome DESC;
 */
