e6619ad5b824c87ca8a4dde4da7bb928e661841f
CaioAReis <Caio.almeida16.ca@gmail.com>
2020-07-20 22:47:11 -0300

    Trabalhando com mongo

--- Aulas/#27-Introducao MongoDB, #28-Mongoose & #29-Config/index.js
+++ Aulas/#27-Introducao MongoDB, #28-Mongoose & #29-Config/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const schema = mongoose.Schema;
 
 //  Configurando o mongoose
     mongoose.Promise = global.Promise;  // --> Linha de código p/ evitar erros
@@ -9,6 +10,25 @@ const mongoose = require('mongoose');
     }).catch((err) => {
         console.log(err);
     });
+
+//  Definindo o Schema
+    const userSchema = mongoose.Schema({
+        name: String,
+        lastName: String,
+        age: Number,
+    });
+//  Definindo a Collection
+const userModel = mongoose.model('users', userSchema);
+//  Criando documento/registro
+new userModel({
+    name: "Caio",
+    lastName: "Almeida",
+    age: 21
+}).save().then(() => {
+    console.log('User created successfully');
+}).catch((err) => {
+    console.log('User not created: err ' + err);
+});
 
 /*  MongoBD
 
@@ -29,11 +49,11 @@ const mongoose = require('mongoose');
 
     --> Mongoose
 
-    Mongoose é uma biblioteca ODM (Object Data Modeling) para MongoDB e Node.js. Ele gerencia relacionamentos 
-    entre dados, fornece validação de esquema e é usado para converter entre objetos no código e a representação 
-    desses objetos no MongoDB.
+        Mongoose é uma biblioteca ODM (Object Data Modeling) para MongoDB e Node.js. Ele gerencia relacionamentos 
+        entre dados, fornece validação de esquema e é usado para converter entre objetos no código e a representação 
+        desses objetos no MongoDB.
 
-    É um módulo que nos permite trabalhar com o mongodb diretamente do node!!
+        É um módulo que nos permite trabalhar com o mongodb diretamente do node!!
 
     INSTALAÇÃO:
 
@@ -45,4 +65,34 @@ const mongoose = require('mongoose');
         mongoose.connect('mongodb://localhost/nomeDoBanco', { useNewUrlParser: true, useUnifiedTopology: true });
 
         { useNewUrlParser: true, useUnifiedTopology: true } --> Serve para evitar um erro de versão que tenho no meu caso.
+
+    DEFININDO SCHEMA
+
+        Tudo no mogoose começa com um esquema(Schema).Cada esquema é mapeado para uma coleção do MongoDB e define
+        a forma dos documentos nessa coleção.
+
+        SEGUINDO EXEMPLO:
+
+        const userSchema = mongoose.Schema({
+            name: String,   --> Uma forma abreviada {type: String}
+            lastNamne: String,
+            email: {type: String, require: true}, --> require: true define que uma informação seja obrigatória
+            idade: Number
+        });
+
+    OS SchemaTypes PERMITIDOS SÃO:
+
+        ->   String      ->   Number      ->   Date        ->   Buffer
+        ->   Boolean     ->   Mixed       ->   ObjectId    ->   Array
+        ->   Decimal128  ->   Map
+
+    CRIANDO UM MODEL
+
+        Para usar a nossa definição de esquema, nós precisamos converter o nosso Schema em um módel. Para fazer
+        isso passamos o seguinte comando: mongoose.model('modelName', schema)
+
+        SEGUINDO EXEMPLO:
+
+            mongoose.model('users', userSchema);
+
 */
