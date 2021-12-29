6a2c1b1405688ca78adc28d086a9c6741b9c2d5e
Caio AReis <Caio.almeida16.ca@gmail.com>
2021-12-29 12:05:08 -0200

    Route POST

--- FS JS/#06 - Routing/src/Routes/user.js
+++ FS JS/#06 - Routing/src/Routes/user.js
@@ -10,4 +10,8 @@ routes.get("/:id", (request, response) => {
     response.send("Você está na rota USER com o ID");
 });
 
+routes.post("/", (request, response) => {
+    response.status(200).json(request.body);
+});
+
 module.exports = routes;
