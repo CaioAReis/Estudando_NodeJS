769570ab96e216b0a688ba5b044f0edcf5e33b3c
Caio AReis <Caio.almeida16.ca@gmail.com>
2021-12-29 22:19:16 -0200

    Rotas PUT e DELETE

--- FS JS/#06 - Routing/src/Routes/user.js
+++ FS JS/#06 - Routing/src/Routes/user.js
@@ -7,11 +7,32 @@ routes.get('/', (request, response) => {
 });
 
 routes.get("/:id", (request, response) => {
-    response.send("Você está na rota USER com o ID");
+    response.send("Você está na rota USER com o ID: " + request.params.id);
 });
 
 routes.post("/", (request, response) => {
-    response.status(200).json(request.body);
+    const data = {
+        "message": "POST request OK",
+        ...request.body
+    };
+    response.status(200).json(data);
+});
+
+routes.put("/:id", (request, response) => {
+    const data = {
+        "message": "PUT request OK",
+        "ID": request.params.id,
+        ...request.body
+    };
+    response.status(200).json(data);
+});
+
+routes.delete("/:id", (request, response) => {
+    const data = {
+        "message": "DELETE request OK",
+        "ID": request.params.id
+    };
+    response.status(200).json(data);
 });
 
 module.exports = routes;
